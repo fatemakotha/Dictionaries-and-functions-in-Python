5beb544455901c6d2beae8ade2e1adcc72145736
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-27 11:20:47 +0000

    Functions with multiple inputs, key-value pair.py

---
+++ 9. Functions with multiple inputs, key-value pair.py
@@ -0,0 +1,10 @@
+#Functions with multiple inputs:
+def greet_with(a, b, c):
+    print(f"Hi {a} !!")
+    print(f"From {b}..")
+    print(f"Hello {c} !!")
+greet_with(a=44, c=99, b=32)
+#prints:
+# Hi 44 !!
+# From 32..
+# Hello 99 !!
