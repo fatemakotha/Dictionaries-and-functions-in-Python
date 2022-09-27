5345ad8fb469aaff36ad1be318daf5c66e510f6e
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-27 11:16:00 +0000

    Functions with multiple inputs

---
+++ 8. Functions with multiple inputs.py
@@ -0,0 +1,9 @@
+#Functions with multiple inputs:
+def greet_with(name, location):
+    print(f"Hi {name} !!")
+    print(f"From {location}..")
+
+greet_with("Kotha", "Dhaka")
+#prints:
+# Hi Kotha !!
+# From Dhaka..
