d1b6cf814ebe762ad8de381edf797b480432f239
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-28 16:45:46 +0000

    Fetching an item from a DICTIONARY using a KEY

---
+++ 16. Fetching an item from a DICTIONARY using a KEY.py
@@ -0,0 +1,8 @@
+#Fetching an item from a DICTIONARY using a KEY
+programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+
+            
+print(programming_dictionary)
+
+bugs = programming_dictionary["Bug"] #prints: An error in a program that prevents the program from running as expected.
+print(bugs)
