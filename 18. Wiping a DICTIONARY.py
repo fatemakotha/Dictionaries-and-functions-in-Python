a79f3e78f48d7dd0be55e1a2baca6ea17ae8903e
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-28 16:53:45 +0000

    Wiping a DICTIONARY

---
+++ 18. Wiping a DICTIONARY.py
@@ -0,0 +1,8 @@
+#Wiping a DICTIONARY
+programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+
+
+programming_dictionary = {}
+print(programming_dictionary)
+#prints:
+# {}
