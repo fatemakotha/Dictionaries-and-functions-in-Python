e649ad3501fee9334ac50cbeda3b09757d5cb51c
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-28 17:01:00 +0000

    Looping through a DICTIONARY

---
+++ 19. Changing an item in a DICTIONARY.py
@@ -0,0 +1,8 @@
+#Loop through a DICTIONARY
+programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+
+programming_dictionary["Bug"] = "just some bugs"
+
+print(programming_dictionary)
+#prints:
+#{'Bug': 'just some bugs', 'Function': 'A piece of code that you can easily call over and over again.'}
