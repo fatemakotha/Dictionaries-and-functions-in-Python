6d788ab1c2f6c73581a031a28b4f2814058413d6
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-28 16:50:44 +0000

    Adding an item to a DICTIONARY

---
+++ 17. Adding an item to a DICTIONARY.py
@@ -0,0 +1,9 @@
+#Adding an item to a DICTIONARY
+programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+
+            
+programming_dictionary["Name"] = "Kotha"
+
+print(programming_dictionary)
+#prints:
+# {'Bug': 'An error in a program that prevents the program from running as expected.', 'Function': 'A piece of code that you can easily call over and over again.', 'Name': 'Kotha'}
